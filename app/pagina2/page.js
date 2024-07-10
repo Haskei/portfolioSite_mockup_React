1d247bbc0e97e7507910796fcf8ca757f3913d34
Pedro Henrrique <89583435+Haskei@users.noreply.github.com>
2024-07-10 14:38:56 +0000

    protótipo 1 versão final

--- app/pagina2/page.js
+++ app/pagina2/page.js
@@ -10,6 +10,17 @@ export default function Pagina2() {
       <div style={{ display: "flex", justifyContent: "center" }}>
         <Image src={profilePic} height={300} alt="Minha Foto" />
       </div>
+      <div>
+        <h3 style={{ display: "flex", justifyContent: "center" }} v>
+          Póscrito
+        </h3>
+      </div>
+      <p>
+        Bom não está perfeito, eu não sei porque o CSS está estranho ao mudar de
+        pagina, e o menu não ta aparecendo direito e eu poderia deixar mais
+        bonito aqui e ali, mas fazendo na mão e sózinho mas
+        <q>done is better than perfect</q> né?
+      </p>
     </>
   );
 }
